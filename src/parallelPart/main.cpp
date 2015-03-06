fd58eab0453ab8a3cdf3d45686853b4aa703d35d
epsilonbelowzero <duda.land@gmail.com>
2015-03-06 14:32:14 +0100

    added command line paramter

--- src/parallelPart/main.cpp
+++ src/parallelPart/main.cpp
@@ -10,23 +10,31 @@
 
 
 
-int main() {
+int main(int argc, char** argv) {
 
-//initialize the particles
-  int len = 5;
-  long double t_start, t_end, dt;
-  particle p = init_1(len);
-  init_params(&t_start, &t_end, &dt);
-  //~ truncateFile();
+    //initialize the particles
+      int len;
+      
+      if( argc == 2) {
+          len = atoi(argv[1]);
+      }
+      else {
+          len = 5;
+      }
+      
+      long double t_start, t_end, dt;
+      particle p = init_1(len);
+      init_params(&t_start, &t_end, &dt);
+      //~ truncateFile();
 
-//start the computation with p
-  compute(t_start, t_end, dt,
-          p.x,p.y,p.z,
-          p.px,p.py,p.pz,
-          p.m,p.q, len);
+    //start the computation with p
+      compute(t_start, t_end, dt,
+              p.x,p.y,p.z,
+              p.px,p.py,p.pz,
+              p.m,p.q, len);
 
 
-  destruct(p);
+      destruct(p);
 
-  return EXIT_SUCCESS;
+      return EXIT_SUCCESS;
 }
