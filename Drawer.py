a7d12d2c8e661454c9382ae3e296231130a39262
epsilonbelowzero <duda.land@gmail.com>
2015-03-06 09:54:17 +0100

    fixed bug - wrong axis-description

--- Drawer.py
+++ Drawer.py
@@ -23,7 +23,7 @@ upperBound = 1 if singleCol else len(data[0])
 for i in range(0, int(upperBound)):
     ax.plot( data[:,i] if not singleCol else data, label="Curve "+str(i+1))
     ax.set_xlabel("$\mathbf{n \cdot dt}$")
-    ax.set_zlabel("$\mathbf{v_x}$")
+    ax.set_ylabel("$\mathbf{v_x}$")
     ax.legend()
 
 plt.show()
