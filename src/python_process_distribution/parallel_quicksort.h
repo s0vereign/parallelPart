38b495a8d7c134c34e901bc545b0b34dc2df6360
dani <dani@dani-pc.fritz.box>
2015-06-18 19:47:53 +0200

    added some docs to the new python-module

--- src/python_process_distribution/parallel_quicksort.h
+++ src/python_process_distribution/parallel_quicksort.h
@@ -7,7 +7,10 @@
 #include <time.h>
 
 //from http://www.oracle.com/technetwork/server-storage/solarisstudio/documentation/oss-openmp-30-wp-183987.pdf
+//parallel quicksort, using OpenMP and its task-directive
 
+//defines the pivot-element, sorting it according to quicksort, returning
+//the position of the pivot-element
 int partition (int p, int r, double *data)
 {
 	double x = data[p];
@@ -39,6 +42,8 @@ int partition (int p, int r, double *data)
 	return l;
 }
 
+//upon a threshold of number of elements is undercut, its no longer
+//recursive but iterative called
 void seq_quick_sort (int p, int r, double *data)
 {
 	if (p < r) {
@@ -47,14 +52,18 @@ void seq_quick_sort (int p, int r, double *data)
 		seq_quick_sort (q + 1, r, data);
 	}
 }
+//standard-function. recursive calls
 void rec_quick_sort (int p, int r, double *data, int low_limit)
 {
 	if (p < r) {
-		if ((r - p) < low_limit) {
+		if ((r - p) < low_limit) {//check, whether the number of elements is lower than treshold
 			seq_quick_sort (p, r, data);
 		}
 		else {
 			int q = partition (p, r, data);
+			//for each call register a new task
+			//the tasks are stored as a stack, so each processor requires
+			//a new task if it's done with his prior one
 			#pragma omp task firstprivate(data, low_limit, r, q)
 				rec_quick_sort (p, q - 1, data, low_limit);
 			#pragma omp task firstprivate(data, low_limit, r, q)
@@ -63,14 +72,19 @@ void rec_quick_sort (int p, int r, double *data, int low_limit)
 	}
 }
 
+//entry-point for the mainprogram. array to be sorted, length and the treshold for iterative quicksort
 void quick_sort (double *data, int n, int low_limit)
 {
 #pragma omp parallel
 {
+	//do only once do the call for the whole array
 	#pragma omp single nowait
 		rec_quick_sort (0, n - 1, data, low_limit);
 }
 }
+
+//old mainprogram for the quicksort. please read the source :
+//http://www.oracle.com/technetwork/server-storage/solarisstudio/documentation/oss-openmp-30-wp-183987.pdf
 
 /*
 
--- src/python_process_distribution/process_distribution.c
+++ src/python_process_distribution/process_distribution.c
@@ -14,6 +14,14 @@
 
 //Doc: https://docs.python.org/3.1/c-api/structures.html#PyMethodDef
 
+/*
+ * The arguments from the call in the appropriate python file results
+ * in a list of args, a PyObject (called args). Here the arguments
+ * are extracted and converted into c-datatypes.
+ * "s|idi" means a obligate string-argument, the others do not needed to
+ * be set. i - integer, d - double
+ * 
+ */
 static int parse_args(PyObject *args, const char** filename, int* serial, double* interval_length, int *interval_count)
 {
     if (!PyArg_ParseTuple(args, "s|idi", filename, &serial, &interval_length, &interval_count)) {
@@ -24,6 +32,15 @@ static int parse_args(PyObject *args, const char** filename, int* serial, double
 	return 0;
 }
 
+/*
+ * This function reads the file given in the arguments, and stores
+ * the data of the format given in Init.h of the mainprogram in the
+ * argument array, also sets the number of entries in the array (lines).
+ * 
+ * If the file is not found, or if its not readable or another error
+ * occurs, python exceptions are thrown.
+ * 
+ */
 static int readFile(char** filename, double **array, int* lines) {
 	
 	//hdf5-stuff
@@ -59,10 +76,18 @@ static int readFile(char** filename, double **array, int* lines) {
     return 0;
 }
 
+/*
+ * This function counts the number of particles which would belong in
+ * a sub-interval of [0, length], storese the number in array result.
+ * count is the length of the result number and the number of subintervals,
+ * data is the position data of the particles with length len (the number
+ * of particles), length denotes the interval-length (usually 2*pi).
+ * Note, that the array data is sorted!
+ */
 static int insertInIntervals(double* data, int len, double length, int count, int** result) {
 	*result = (int*) malloc(sizeof(int) * count);
 	
-	int k = 0,
+	int k = 0, //caches the position of the last particle of the lower subinterval
 		j;
 	
 	for(int i = 0; i < count; i++) {	
@@ -76,49 +101,72 @@ static int insertInIntervals(double* data, int len, double length, int count, in
 	return 0;
 }
 
+/*
+ * Main-function, which is called by the interpreter and the main-program.
+ * 
+ */
 static PyObject *process(PyObject *self, PyObject *args) {
 	
-	const char* filename;//reserve memory?
+	//the parameters and their defaults are set
+	const char* filename;
 	int serial = 1000;
 	double length = 2 * M_PI;
 	int count = 1000;
 	int return_code;
 	
+	//arguments parsed. if an error occurred, an exception is already
+	//thrown, and NULL is returned
 	return_code = parse_args(args, &filename, &serial, &length, &count);
 	if( return_code < 0 ) {
 		return NULL;
 	}
 
+	//defines the array which will contain the positions of the particles
 	double* array;
 	int lines;
+	//reads the data from the given file and stores it in the array
+	//return NULL if an error occurred, an exception is already thrown
+	//in this case
 	return_code = readFile(&filename, &array, &lines);
 	if( return_code < 0) {
 		return NULL;
 	}
 	
+	//sorts the array. serial is a treshold where no longer is
+	//recursive but iterative sorted
 	printf("Sorting\n");
 	quick_sort(array, lines, serial);
 	
+	//counts the number of particles which belong in each subinterval,
+	//stores the result in array result
 	printf("Seperating\n");
 	int* result;
 	insertInIntervals(array, lines, length, count, &result);
-	free(array);
+	free(array); //position data of the particles are no longer needed
 	
 	//generate numpy-array
-	int nd = 1; //1 dimension
+	//as python cannot handle c-arrays, it needs to be converted into
+	//an object python can handle. numpy has the advantage, that the
+	//c-array dont needs to be extracted into a list (so iterating over
+	//each element) and an option exists that numpy releases the memory
+	//when its no longer needed
+	int nd = 1; //1 dimensional
 	npy_intp dims[nd];
-	dims[0] = count;
+	dims[0] = count;//length of the dimension
 	
+	//create numpy array.
+	//first argument is firm, number of array-dimensions, length of each array-dimension, data-type, NULL, the array to convert, 0, c-array / it can be written, NULL
 	PyObject* numpylist = PyArray_New(&PyArray_Type, nd, dims, NPY_INT, NULL, result, 0, NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_WRITEABLE, NULL);
 	
 	if( numpylist == NULL ) {
 		return NULL;
 	}
+	//mark the array as owned, so the memory is released after its no longer needed
 	PyArray_ENABLEFLAGS(numpylist, NPY_ARRAY_OWNDATA);
 	
 	return numpylist;
 }
-
+//array of structs of information of each funtion callable of the interpreter.
 static struct PyMethodDef PyInit_process_distribution_methods[] = {{
    "process",
    process,
@@ -127,6 +175,7 @@ static struct PyMethodDef PyInit_process_distribution_methods[] = {{
 }};
 
 //Doc: https://docs.python.org/3.1/c-api/module.html#PyModuleDef
+//moduleinformation
 static PyModuleDef PyInit_process_distribution_module = { 
 	PyModuleDef_HEAD_INIT,
     "read_sort",
@@ -139,15 +188,17 @@ static PyModuleDef PyInit_process_distribution_module = {
     NULL
 };
 
+//function which is called when the module is loaded.
 PyMODINIT_FUNC
 PyInit_process_distribution(void)
 {
+	//create the module with the information given above
 	PyObject* m = PyModule_Create(&PyInit_process_distribution_module);
 	if( m == NULL) {
 		return NULL;
 	}
 	
-	import_array()
+	import_array(); //initialise numpy - else its functions could not be used
 	
 	return m;
 }
