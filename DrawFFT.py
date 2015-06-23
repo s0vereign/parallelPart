cf2ccaa842da65cd6c07e0998f065ae4973ad1a0
dani <dani@dani-pc.fritz.box>
2015-06-23 13:26:46 +0200

    DrawFFT.py now also draws the absolute value of Re and Im

--- DrawFFT.py
+++ DrawFFT.py
@@ -20,8 +20,11 @@ fig = plt.figure()
 
 ax = fig.add_subplot(111)
 
+print("Data-Length: " + str(len(data[:,0])))
+
 ax.plot(data[:,0])
 ax.plot(data[:,1])
+ax.plot(np.sqrt(data[:,0]**2 + data[:,1]**2))
 ax.set_xlabel("$\mathbf{\omega}$")
 ax.set_ylabel("$\~I^p(\omega)$")
 plt.show()
